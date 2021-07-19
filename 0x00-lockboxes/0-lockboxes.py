624b56aa1de618b68a38c7d766d90ed0983b364d
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-19 15:50:03 +0000

    Add task 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,6 +3,22 @@
 Each box is numbered sequentially from 0 to n - 1 and each box
 may contain keys to the other boxes. """
 
+def join(T,R):
+    res = []
+    for e in R:
+        res += T[e]
+    return res
+
 def canUnlockAll(boxes):
-""" method that determines if lockboxes can be opened """
-    
+    """ method that determines if lockboxes can be opened """
+    index = 0
+    total = list(set(boxes[0]) | {0})
+    added = True
+    while added:
+        added = False
+        for j in join(boxes, total[index:]):
+            if j not in total:
+                total.append(j)
+                index +=1
+                added = True
+    return len(total) == len(boxes)
