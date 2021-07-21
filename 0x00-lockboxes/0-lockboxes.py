484c33ee3d8cf18c78259ea96b07eb18d4c399a8
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-21 07:40:56 -0500

    Add new version

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,24 +3,31 @@
 Each box is numbered sequentially from 0 to n - 1 and each box
 may contain keys to the other boxes. """
 
-
-def join(T, R):
-    res = []
-    for e in R:
-        res.append(T[e])
-    return res
+#!/usr/bin/python3
+"""Determines if all boxes can be opened, beginning with 0"""
 
 
 def canUnlockAll(boxes):
-    """ method that determines if lockboxes can be opened """
-    index = 0
-    total = list(set(boxes[0]) | {0})
-    added = True
-    while added:
-        added = False
-        for j in join(boxes, total[index:]):
-            if j not in total:
-                total.append(j)
-                index += 1
-                added = True
-    return len(total) == len(boxes)
+    keysDict = {
+        0: True
+    }
+    # number of times going through the loop
+    # need to keep track of this for while loop condition (to prevent infinite loop)
+    loopIterations = 0
+
+    # populate the rest of the dict to False values to start
+    for i in range(1, len(boxes)):
+        keysDict[i] = False
+
+    # loop through all keys
+    while False in keysDict.values() and trips < len(boxes):
+        for i in keysDict:
+            if keysDict[i] == True:
+                for k in boxes[i]:
+                    keysDict[k] = True
+                loopIterations += 1
+
+    # if there is still a False value anywhere in the dict, method returns false
+    if False in keysDict.values():
+        return False
+    return True
