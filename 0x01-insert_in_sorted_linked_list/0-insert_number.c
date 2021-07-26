2dbb43a186b4723ccea01a8533163b29928f5396
valerienierenberg <valerie.nierenberg@gmail.com>
2021-07-26 15:26:54 +0000

    typo

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -26,7 +26,7 @@ listint_t *insert_node(listint_t **head, int number)
 		}
 	}
 	/*key node found and insert new node */
-	p = malloc(sizeof(listint_t));  /* space fornew node */
+	p = malloc(sizeof(listint_t));  /* space for new node */
 	p->n = number; /* place value in the new node */
 	p->next = p2; /*link new node to key node */
 	if (p1 == NULL)
