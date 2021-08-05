4e31abeca61c84c235be88f788048ca83bbee1d6
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-05 15:58:17 +0000

    comment out print statements

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -17,7 +17,7 @@ heap_t *heap_insert(heap_t **root, int value)
 
 	if (*root == NULL)
 	{
-		printf("first");
+		/* printf("first"); */
 		new->n = value;
 		new->parent = new->left = new->right = NULL;
 		*root = new;
@@ -27,7 +27,7 @@ heap_t *heap_insert(heap_t **root, int value)
 	{
 		if (current->left == NULL)
 		{
-			printf("got here\n");
+			/* printf("got here\n"); */
 			current->left = new;
 			new->n = value;
 			new->parent = current;
@@ -35,7 +35,7 @@ heap_t *heap_insert(heap_t **root, int value)
 
 			if (value > current->n)
 			{
-				printf("value > current->n\n");
+				/* printf("value > current->n\n"); */
 				current = new;
 				return (new);
 			}
