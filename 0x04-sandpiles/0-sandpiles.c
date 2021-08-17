33acc1290b76ffb1b02b551e5786a9c5708ae257
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-17 14:50:26 +0000

    At the point where I'm adding the two matrices together successfully

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -9,5 +9,14 @@
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    
+    int i, j;
+    /* int newgrid[3][3]; */
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            grid1[i][j] = grid1[i][j] + grid2[i][j];
+        }
+    }
 }
