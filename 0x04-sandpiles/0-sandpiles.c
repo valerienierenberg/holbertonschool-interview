0d99890b61983e2063f6e5b90d0a55dec8fc7341
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-17 08:56:52 -0500

    Add README and bones of task files

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,13 @@
+#include "sandpiles.h"
+
+/**
+* sandpiles_sum- computes the sum of two sandpiles
+* @grid1: first sandpile, matrix of ints
+* @grid2: second sandpile, matrix of ints
+* Return: sum of two sandpiles
+*/
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    
+}
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,11 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* SANDPILES_H */
