6b4f533679d81f284bf673571fdfd38cb27ead8b
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-18 17:43:42 +0000

    Add

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -19,6 +19,8 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
             grid1[i][j] = grid1[i][j] + grid2[i][j];
         }
     }
+    /*printf("=\n");*/
+    /*print_grid1(grid1);*/
 
     for (i = 0; i < 3; i++)
     {
@@ -40,12 +42,35 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
             if (num >= 4)
             {
                 newgrid[i][j] = grid1[i][j] - 4;
-                newgrid[i + 1][j]++;
-                newgrid[i - 1][j]++;
-                newgrid[i][j + 1]++;
-                newgrid[i][j - 1]++;
+                if (i + 1 < 3)
+                    newgrid[i + 1][j]++;
+                if (i - 1 >= 0)
+                    newgrid[i - 1][j]++;
+                if (j + 1 < 3)
+                    newgrid[i][j + 1]++;
+                if (j - 1 >= 3)
+                    newgrid[i][j - 1]++;
             }
         }
     }
+    printf("=\n");
+    print_grid1(newgrid);
     grid1 = newgrid;
+}
+
+
+void print_grid1(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
