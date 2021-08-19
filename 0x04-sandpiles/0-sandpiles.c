4e7a950a84f7b457eb1d01d5ac7eeaf2d8a0896e
valerienierenberg <68240233+valerienierenberg@users.noreply.github.com>
2021-08-19 07:57:08 -0500

    Update 0-sandpiles.c

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -18,11 +18,11 @@ void add_sandies(int grid1[3][3], int grid2[3][3])
 		for (j = 0; j < 3; j++)
 			grid1[i][j] = grid1[i][j] + grid2[i][j];
 	}
-	if (check_stable(grid1) == 0)
+	/*if (check_stable(grid1) == 0)
 	{
 		printf("=\n");
 		print_grid1(grid1);
-	}
+	}*/
 }
 
 /**
@@ -34,8 +34,7 @@ void add_sandies(int grid1[3][3], int grid2[3][3])
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j, num;
-	int newgrid[3][3];
+	int i, j, num, check, newgrid[3][3];
 
 	add_sandies(grid1, grid2);
 	for (i = 0; i < 3; i++)
@@ -47,14 +46,17 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 				newgrid[i][j] = grid1[i][j];
 		}
 	}
-	for (i = 0; i < 3; i++)
+	check = check_stable(grid1);
+	while (check == 1)
 	{
-		for (j = 0; j < 3; j++)
+		print_grid1(grid1);
+		for (i = 0; i < 3; i++)
 		{
-			num = grid1[i][j];
-			if (num >= 4)
+			for (j = 0; j < 3; j++)
 			{
-				newgrid[i][j] = grid1[i][j] - 4;
+				num = grid1[i][j];
+				if (num >= 4)
+					newgrid[i][j] = grid1[i][j] - 4;
 				if (i + 1 < 3)
 					newgrid[i + 1][j]++;
 				if (i - 1 >= 0)
@@ -63,15 +65,17 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 					newgrid[i][j + 1]++;
 				if (j - 1 >= 3)
 					newgrid[i][j - 1]++;
+				}
 			}
 		}
-	}
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
+		for (i = 0; i < 3; i++)
 		{
-			grid1[i][j] = newgrid[i][j];
+			for (j = 0; j < 3; j++)
+			{
+				grid1[i][j] = newgrid[i][j];
+			}
 		}
+		check = check_stable(grid1);
 	}
 }
 
