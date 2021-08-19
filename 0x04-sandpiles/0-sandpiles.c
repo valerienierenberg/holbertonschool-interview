e9dcdb9e716143348bde241bf234d85b7423c5fb
valerienierenberg <68240233+valerienierenberg@users.noreply.github.com>
2021-08-19 08:21:38 -0500

    Update 0-sandpiles.c

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -55,7 +55,6 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 			{
 				if (grid1[i][j] >= 4)
 					newgrid[i][j] -= 4;
-				/*
 				if (i != 0 && grid1[i - 1][j] >= 4)
 					newgrid[i][j]++;
 				if (i != 2 && grid1[i + 1][j] >= 4)
@@ -63,15 +62,7 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 				if (j != 0 && grid1[i][j - 1] >= 4)
 					newgrid[i][j]++;
 				if (j != 2 && grid1[i][j + 1] >= 4)
-					newgrid[i][j]++;*/
-				if (i + 1 < 3)
-					newgrid[i + 1][j]++;
-				if (i - 1 >= 0)
-					newgrid[i - 1][j]++;
-				if (j + 1 < 3)
-					newgrid[i][j + 1]++;
-				if (j - 1 >= 0)
-					newgrid[i][j - 1]++;
+					newgrid[i][j]++;
 			}
 		}
 		for (i = 0; i < 3; i++)
