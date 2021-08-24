073db32afaa4b3070a765350fdbe2f354d5bdf03
valerienierenberg <valerie.nierenberg@gmail.com>
2021-08-24 14:03:43 +0000

    Add task

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+* is_palindrome - checks if a singly linked list is a palindrome
+* @head: pointer to head of linked list
+* empty list is considered a palindrome
+* Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
+
+int is_palindrome(listint_t **head)
+{
+	if (!*head)
+		return (1);
+		return (pal_meat(head, *head));
+}
+
+
+/**
+* pal_meat - helper function to check if list is palindrome
+* @left: start of list
+* @right: end of list
+* empty list is considered a palindrome
+* Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
+
+int pal_meat(listint_t **left, listint_t *right)
+{
+	if (!right)
+		return (1);
+	if (pal_meat(left, right->next) && (*left)->n == right->n)
+	{
+		*left = (*left)->next;
+		return (1);
+	}
+	else
+		return (0);
+}
--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -20,5 +23,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+int pal_meat(listint_t **left, listint_t *right);
 
 #endif /* LISTS_H */
