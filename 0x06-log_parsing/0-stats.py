b9157d4f9599d8740473382e9ef7338527e444f4
valerienierenberg <valerie.nierenberg@gmail.com>
2021-09-01 15:52:34 +0000

    Add continue

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -22,9 +22,9 @@ if __name__ == '__main__':
             input_line = line.rstrip()
 
             if "Hello" in input_line:
-                pass
+                continue
             if "Holberton" in input_line:
-                pass
+                continue
             filesize = input_line.rsplit(' ', 1)[1]
             statuscode = int(input_line.rsplit(' ', 2)[1])
             count += 1
