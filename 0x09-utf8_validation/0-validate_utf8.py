c59a9e5d6caf0bba89d0b4fe3931bd8bc2a9151a
valerienierenberg <valerie.nierenberg@gmail.com>
2021-09-23 18:56:26 -0500

    add

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -9,13 +9,12 @@ def validUTF8(data):
         while index < len(data):
             number = data[index] & (2 ** 7)
             number >>= (NUMBER_OF_BITS_PER_BLOCK - 1)
-            if number == 0:  # single byte char
+            if number == 0:
                 index += 1
                 continue
 
-            # validate multi-byte char
             number_of_ones = 0
-            while True:  # get the number of significant ones
+            while True:
                 number = data[index] & (2 ** (7 - number_of_ones))
                 number >>= (NUMBER_OF_BITS_PER_BLOCK - number_of_ones - 1)
                 if number == 1:
@@ -24,18 +23,16 @@ def validUTF8(data):
                     break
 
                 if number_of_ones > MAX_NUMBER_OF_ONES:
-                    return False  # too much ones per char sequence
+                    return False
 
             if number_of_ones == 1:
-                return False  # there has to be at least 2 ones
+                return False
 
-            index += 1  # move on to check the next byte in a multi-byte char sequence
+            index += 1
 
-            # check for out of bounds and exit early
             if index >= len(data) or index >= (index + number_of_ones - 1):
                 return False  
 
-            # every next byte has to start with "10"
             for i in range(index, index + number_of_ones - 1):
                 number = data[i]
 
