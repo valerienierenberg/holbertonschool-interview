106d1c435dfa4801e6c0407ae7b01dc2686c2720
valerienierenberg <valerie.nierenberg@gmail.com>
2021-09-23 19:00:31 -0500

    Indent

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,45 +5,45 @@ UTF-8 encoding. """
 
 def validUTF8(data):
     """ validUTF8 method """
-        index = 0
-        while index < len(data):
-            number = data[index] & (2 ** 7)
+    index = 0
+    while index < len(data):
+        number = data[index] & (2 ** 7)
+        number >>= (7)
+        if number == 0:
+            index += 1
+            continue
+
+        number_of_ones = 0
+        while True:
+            number = data[index] & (2 ** (7 - number_of_ones))
+            number >>= (8 - number_of_ones - 1)
+            if number == 1:
+                number_of_ones += 1
+            else:
+                break
+
+            if number_of_ones > 4:
+                return False
+
+        if number_of_ones == 1:
+            return False
+
+        index += 1
+
+        if index >= len(data) or index >= (index + number_of_ones - 1):
+            return False  
+
+        for i in range(index, index + number_of_ones - 1):
+            number = data[i]
+
             number >>= (7)
-            if number == 0:
-                index += 1
-                continue
-
-            number_of_ones = 0
-            while True:
-                number = data[index] & (2 ** (7 - number_of_ones))
-                number >>= (8 - number_of_ones - 1)
-                if number == 1:
-                    number_of_ones += 1
-                else:
-                    break
-
-                if number_of_ones > 4:
-                    return False
-
-            if number_of_ones == 1:
+            if number != 1:
+                return False
+            number >>= (7)
+            if number != 0:
                 return False
 
             index += 1
 
-            if index >= len(data) or index >= (index + number_of_ones - 1):
-                return False  
-
-            for i in range(index, index + number_of_ones - 1):
-                number = data[i]
-
-                number >>= (7)
-                if number != 1:
-                    return False
-                number >>= (7)
-                if number != 0:
-                    return False
-
-                index += 1
-
-        return True
+    return True
 
