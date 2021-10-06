c3b62f4d67ad3e23fda8281c1e01d9c778d1757a
valerienierenberg <valerie.nierenberg@gmail.com>
2021-10-06 16:17:07 +0000

    Add prototype for 4

--- 0x0B-menger/menger.h
+++ 0x0B-menger/menger.h
@@ -5,23 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-/**
- * struct sCarpet - doubly linked list
- * @dim: dimensions
- * @data: character data
- * @rows: number of rows
- */
-typedef struct sCarpet
-{
-	int dim;
-	char *data;
-	char **rows;
-} *Carpet;
-
 void menger(int level);
-void TileCarpet(Carpet d, int r, int c, Carpet tile);
-Carpet Sierpinski(int level);
-void CarpetPrint(Carpet carp);
+void print4menger();
 
 #endif
