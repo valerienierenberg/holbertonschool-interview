ae1e2876e835c4ee6ab40ee378a4092111cef475
valerienierenberg <valerie.nierenberg@gmail.com>
2021-10-07 20:50:05 -0500

    Add

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -9,36 +9,36 @@
 
 void menger(int level)
 {
-    int width, height, z = pow(3, level);
-    int check;
-    char printchar;
+	int width, height, z = pow(3, level);
+	int check;
+	char printchar;
 
-    if (level < 0)
-    {
-        ;
-    }
-    if (level == 0)
-    {
-        printf("#\n");
-        return;
-    }
-    for (height = 0; height < z; height++)
-    {
-        for (width = 0; width < z; width++)
-        {
-            check = 1;
-            printchar = '#';
-            for (; check < z; check *= 3)
-            {
-                if ((height / check) % 3 == 1 &&
-                    (width / check) % 3 == 1)
-                    {
-                       printchar = ' ';
-                       break;
-                    }
-            }
-            printf("%c", printchar);
-        }
-        printf("\n");
-    }
+	if (level < 0)
+	{
+		;
+	}
+	if (level == 0)
+	{
+		printf("#\n");
+		return;
+	}
+	for (height = 0; height < z; height++)
+	{
+		for (width = 0; width < z; width++)
+		{
+			check = 1;
+			printchar = '#';
+			for (; check < z; check *= 3)
+			{
+				if ((height / check) % 3 == 1 &&
+					(width / check) % 3 == 1)
+				{
+					printchar = ' ';
+					break;
+				}
+			}
+			printf("%c", printchar);
+		}
+		printf("\n");
+	}
 }
