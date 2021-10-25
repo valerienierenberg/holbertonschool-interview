1bbd96bca44b7391adcdf1da94b7d6a8b61b2995
valerienierenberg <valerie.nierenberg@gmail.com>
2021-10-25 18:36:52 +0000

    add

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -14,7 +14,7 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 
     if (array == NULL)
         return (NULL);
-    for (i = 0; i < size; i++)
+    for (i = 0; i < (int)size; i++)
     {
         root = avl_insert(root, array[i]);
     }
@@ -37,7 +37,7 @@ avl_t *avl_insert(avl_t *root, int value)
     new_node->n = value;
     new_node->left = NULL;
     new_node->right = NULL;
-    new_node->height = 0;
+    /*new_node->height = 0;*/
     if (root == NULL)
         return (new_node);
     temp = root;
