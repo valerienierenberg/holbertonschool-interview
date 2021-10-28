c5162b53fd5b07a061b0b26283c7f744ae6ccfaf
valerienierenberg <valerie.nierenberg@gmail.com>
2021-10-28 20:42:36 +0000

    betty fixes

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -9,16 +9,16 @@
 
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    avl_t *root = NULL;
-    int i;
+	avl_t *root = NULL;
+	int i;
 
-    if (array == NULL)
-        return (NULL);
-    for (i = 0; i < (int)size; i++)
-    {
-        root = avl_insert(root, array[i]);
-    }
-    return (root);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < (int)size; i++)
+	{
+		root = avl_insert(root, array[i]);
+	}
+	return (root);
 }
 
 /**
@@ -29,38 +29,38 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
  */
 avl_t *avl_insert(avl_t *root, int value)
 {
-    avl_t *new_node, *temp;
+	avl_t *new_node, *temp;
 
-    new_node = malloc(sizeof(avl_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->n = value;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    /*new_node->height = 0;*/
-    if (root == NULL)
-        return (new_node);
-    temp = root;
-    while (temp != NULL)
-    {
-        if (value < temp->n)
-        {
-            if (temp->left == NULL)
-            {
-                temp->left = new_node;
-                break;
-            }
-            temp = temp->left;
-        }
-        else
-        {
-            if (temp->right == NULL)
-            {
-                temp->right = new_node;
-                break;
-            }
-            temp = temp->right;
-        }
-    }
-    return (root);
+	new_node = malloc(sizeof(avl_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	/*new_node->height = 0;*/
+	if (root == NULL)
+		return (new_node);
+	temp = root;
+	while (temp != NULL)
+	{
+		if (value < temp->n)
+		{
+			if (temp->left == NULL)
+			{
+				temp->left = new_node;
+				break;
+			}
+			temp = temp->left;
+		}
+		else
+		{
+			if (temp->right == NULL)
+			{
+				temp->right = new_node;
+				break;
+			}
+			temp = temp->right;
+		}
+	}
+	return (root);
 }
