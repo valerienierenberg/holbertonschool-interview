0b912effdcff9fdde82bedbb4fba302635b80454
valerienierenberg <valerie.nierenberg@gmail.com>
2021-11-19 12:26:21 +0000

    Add setup task files

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,14 @@
+#include <sort.h>
+
+/**
+* heap_sort - function that sorts an array of integers in ascending
+* order using the (sift down) Heap sort algorithm
+* @array: integers
+* @size: size of the array
+* Return: void
+*/
+
+void heap_sort(int *array, size_t size)
+{
+
+}
---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+
+#endif
