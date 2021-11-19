9a4cc49692f2da22cf88bae46c02d8006e407ce1
valerienierenberg <valerie.nierenberg@gmail.com>
2021-11-19 06:34:35 -0600

    Add function

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -10,5 +10,21 @@
 
 void heap_sort(int *array, size_t size)
 {
+    int i, j, temp;
 
+    if (array == NULL || size < 2)
+        return;
+
+    for (i = size / 2 - 1; i >= 0; i--)
+    {
+        sift_down(array, i, size);
+    }
+
+    for (i = size - 1; i > 0; i--)
+    {
+        temp = array[0];
+        array[0] = array[i];
+        array[i] = temp;
+        sift_down(array, 0, i);
+    }
 }
