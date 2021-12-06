e6aed97092e23813c5228053727e242714b76ef9
valerienierenberg <valerie.nierenberg@gmail.com>
2021-12-06 06:56:10 -0600

    Add

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -7,18 +7,18 @@ import requests
 def count_words(subreddit, word_list):
     """recursive function that queries the Reddit API and returns
     a count of all hot articles for a given subreddit"""
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    header = {'User-Agent': 'My User Agent 1.0'}
-    params = {'limit': 100}
-    response = requests.get(url, headers=header, params=params)
-    if response.status_code == 404:
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    params = {"limit": 100}
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code != 200:
         return 0
-    response_json = response.json()
-    children = response_json.get('data').get('children')
-    titles = [child.get('data').get('title') for child in children]
-    for word in word_list:
-        count = titles.count(word)
-        print("{}: {}".format(word, count))
-    after = response_json.get('data').get('after')
-    if after:
-        return count_words(subreddit, word_list)
+    data = response.json()
+    children = data.get("data").get("children")
+    for child in children:
+        title = child.get("data").get("title")
+        for word in word_list:
+            if word in title:
+                print("{}: {}".format(word, title))
+    if data.get("data").get("after") is not None:
+        count_words(subreddit, word_list)
