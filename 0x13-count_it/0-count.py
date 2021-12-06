066a9f54a26c44ab949c96b1ff021bc8f633f049
valerienierenberg <valerie.nierenberg@gmail.com>
2021-12-06 06:44:16 -0600

    change to count

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
 """recursive function that queries the Reddit API and returns
-a list containing the titles of all hot articles for a given
-subreddit. If no results are found for the given subreddit, the
-function should return None."""
-import json
+a count of all hot articles for a given subreddit"""
 import requests
-from sys import argv
 
 
 def count_words(subreddit, word_list):
-    """returns the number of times a word is used in a subreddit"""
+    """recursive function that queries the Reddit API and returns
+    a count of all hot articles for a given subreddit"""
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
-    params = {'limit': 100}
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code != 200:
-        return None
-    data = response.json()
-    children = data.get('data').get('children')
-    for child in children:
-        title = child.get('data').get('title')
-        words = title.split()
-        for word in words:
-            if word.lower() in word_list:
-                word_list[word.lower()] += 1
-    after = data.get('data').get('after')
-    if after is not None:
-        params['after'] = after
-        count_words(subreddit, word_list)
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 404:
+        return 0
+    else:
+        try:
+            data = r.json()
+            for post in data['data']['children']:
+                for word in word_list:
+                    if word in post['data']['title'].lower():
+                        return 1 + count_words(subreddit, word_list)
+        except:
+            return 0
+
