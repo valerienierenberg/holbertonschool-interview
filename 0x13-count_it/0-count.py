1a7e2304be3cb645e901b59f569f718739db60e9
valerienierenberg <valerie.nierenberg@gmail.com>
2021-12-06 07:07:41 -0600

    Add

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -4,22 +4,29 @@ a count of all hot articles for a given subreddit"""
 import requests
 
 
-def count_words(subreddit, word_list, hot_list=[], after=None):
+def count_words(subreddit, word_list, after=None):
     """recursive function that queries the Reddit API and returns
     a count of all hot articles for a given subreddit"""
     url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit,
                                                                 after)
-    headers = {'User-Agent': 'custom'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                             'AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/76.0.3809.100 Safari/537.36'}
     r = requests.get(url, headers=headers, allow_redirects=False)
     if r.status_code != 200:
         return 0
-    r_json = r.json()
-    data = r_json.get('data')
-    children = data.get('children')
-    after = data.get('after')
-    for i in children:
-        hot_list.append(i.get('data').get('title'))
+    data = r.json()
+    posts = data.get('data').get('children')
+    count = 0
+    for post in posts:
+        if word_list is None:
+            return count
+        post_data = post.get('data')
+        title = post_data.get('title')
+        for word in word_list:
+            if word in title:
+                count += 1
+    after = data.get('data').get('after')
     if after is None:
-        return 0
-    return count_words(subreddit, word_list, hot_list, after)
-
+        return count
+    return count + count_words(subreddit, word_list, after)
