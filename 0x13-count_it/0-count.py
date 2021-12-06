39b8e2a7587a59bb601f814e329a9ea26248d077
valerienierenberg <valerie.nierenberg@gmail.com>
2021-12-06 07:08:49 -0600

    Add

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -4,29 +4,26 @@ a count of all hot articles for a given subreddit"""
 import requests
 
 
-def count_words(subreddit, word_list, after=None):
-    """recursive function that queries the Reddit API and returns
-    a count of all hot articles for a given subreddit"""
-    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit,
-                                                                after)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                             'AppleWebKit/537.36 (KHTML, like Gecko) '
-                             'Chrome/76.0.3809.100 Safari/537.36'}
-    r = requests.get(url, headers=headers, allow_redirects=False)
-    if r.status_code != 200:
+def count_words(subreddit, word_list, hot_list=[]):
+    """recursive function that queries the Reddit API and
+    returns a count of all hot articles for a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "My-User-Agent"
+    }
+    params = {
+        "limit": "100"
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code != 200:
         return 0
-    data = r.json()
-    posts = data.get('data').get('children')
-    count = 0
-    for post in posts:
-        if word_list is None:
-            return count
-        post_data = post.get('data')
-        title = post_data.get('title')
-        for word in word_list:
-            if word in title:
-                count += 1
-    after = data.get('data').get('after')
-    if after is None:
-        return count
-    return count + count_words(subreddit, word_list, after)
+    data = response.json()
+    children = data.get("data").get("children")
+    for child in children:
+        hot_list.append(child.get("data").get("title"))
+    for word in word_list:
+        for hot in hot_list:
+            if word in hot:
+                return hot_list.count(word)
+    return 0
