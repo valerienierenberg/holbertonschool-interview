d1c0efc0603129d2d90c7f1bf04f7f8edbb06d3f
valerienierenberg <valerie.nierenberg@gmail.com>
2021-12-06 06:45:50 -0600

    change to count

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -7,18 +7,18 @@ import requests
 def count_words(subreddit, word_list):
     """recursive function that queries the Reddit API and returns
     a count of all hot articles for a given subreddit"""
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'My User Agent 1.0'}
-    r = requests.get(url, headers=headers, allow_redirects=False)
-    if r.status_code == 404:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
-    else:
-        try:
-            data = r.json()
-            for post in data['data']['children']:
-                for word in word_list:
-                    if word in post['data']['title'].lower():
-                        return 1 + count_words(subreddit, word_list)
-        except:
-            return 0
-
+    data = response.json()
+    count = 0
+    for post in data['data']['children']:
+        title = post['data']['title']
+        for word in word_list:
+            if word in title:
+                count += 1
+    if data['data']['after'] is not None:
+        count += count_words(subreddit, word_list)
+    return count
