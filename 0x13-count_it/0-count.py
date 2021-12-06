77c0e5ed7fddfc3d976df26be1b6fd2aac570178
valerienierenberg <valerie.nierenberg@gmail.com>
2021-12-06 07:01:00 -0600

    Add

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -7,20 +7,21 @@ import requests
 def count_words(subreddit, word_list):
     """recursive function that queries the Reddit API and returns
     a count of all hot articles for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?after=t3_".format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    response = requests.get('https://www.reddit.com/r/{}/hot.json?limit=100'.
+                            format(subreddit), headers={'User-Agent': 'custom'},
+                            allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    data = response.json()
-    posts = data.get('data').get('children')
-    if not posts:
-        return 0
-    for post in posts:
-        title = post.get('data').get('title')
-        for word in word_list:
-            if word in title:
-                count += 1
-    if data.get('data').get('after'):
-        return count + count_words(subreddit, word_list)
-    return count
+    else:
+        data = response.json()
+        posts = data['data']['children']
+        count = 0
+        for post in posts:
+            title = post['data']['title']
+            for word in word_list:
+                if word in title.lower():
+                    count += 1
+        if count == 0:
+            return 0
+        else:
+            return count + count_words(subreddit, word_list)
