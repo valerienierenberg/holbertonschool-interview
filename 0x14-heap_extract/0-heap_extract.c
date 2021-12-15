71590f09af3027f7dfa29f325174dc250c6699f2
valerienierenberg <valerie.nierenberg@gmail.com>
2021-12-15 16:54:05 -0600

    try

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -3,50 +3,60 @@
 /**
 * heap_extract - extract the root of a heap
 * @root: root of the heap
+* Your function must return the value stored in the root node
+* The root node must be freed and replace with the last level-order node of the heap
+* Once replaced, the heap must be rebuilt if necessary
+* Return: the value of the root node
 */
 int heap_extract(heap_t **root)
 {
-	int data;
-	heap_t *temp;
+    heap_t *temp;
+    int value;
 
-	if (!root || !(*root))
-		return (0);
-	temp = *root;
-	data = temp->n;
-	if (temp->left)
-		heap_insert(&temp->left, temp->left);
-	if (temp->right)
-		heap_insert(&temp->right, temp->right);
-	*root = temp->left;
-	free(temp);
-	return (data);
+    if (!root || !*root)
+        return (0);
+
+    temp = *root;
+    value = temp->n;
+    *root = temp->left;
+    free(temp);
+    if (*root)
+        heapify(root);
+    return (value);
 }
 
-int heap_insert(heap_t **root, heap_t *node)
+void heapify(heap_t **root)
+{
+    heap_t *temp;
+    heap_t *parent;
+    heap_t *child;
+
+    temp = *root;
+    parent = temp->parent;
+    child = temp->left;
+    while (child)
+    {
+        if (child->n > temp->n)
+        {
+            swap_nodes(&temp, &child);
+            temp = child;
+            child = temp->left;
+        }
+        else
+            break;
+    }
+    if (parent && temp->n > parent->n)
+    {
+        swap_nodes(&temp, &parent);
+        heapify(&parent);
+    }
+}
+
+void swap_nodes(heap_t **a, heap_t **b)
 {
     heap_t *temp;
 
-    if (!root || !node)
-        return (-1);
-    if (!(*root))
-    {
-        *root = node;
-        return (1);
-    }
-    temp = *root;
-    if (node->n > temp->n)
-    {
-        node->right = temp;
-        *root = node;
-    }
-    else
-    {
-        while (temp->right && temp->right->n < node->n)
-            temp = temp->right;
-        if (temp->right)
-            temp->right->left = node;
-        node->right = temp->right;
-        temp->right = node;
-    }
-    return (1);
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
