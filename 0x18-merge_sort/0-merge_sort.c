e4595bcbb589c5ba11b9a6a43aec71bc621fb48e
valerienierenberg <valerie.nierenberg@gmail.com>
2022-01-20 19:02:12 -0600

    Add

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -1,58 +1,36 @@
 #include "sort.h"
 
 /**
-* merge_sort - sorts an array of ints using top-down merge sort algorithm
-* @array: array to be sorted
-* @size: size of array
+* merge_sort - Merge sort
+* @array: Array to sort
+* @size: Size of array
 */
+
 void merge_sort(int *array, size_t size)
 {
+	size_t i, j, k;
+	int *temp;
+
 	if (size < 2)
 		return;
-	merge_sort(array, size / 2);
-	merge_sort(array + (size / 2), size - (size / 2));
-	merge(array, size);
-}
 
-/**
-* merge - merges two sorted arrays into one
-* @array: array to be sorted
-* @size: size of array
-*/
-void merge(int *array, size_t size)
-{
-	int *temp = malloc(sizeof(int) * size);
-	size_t i = 0, j = 0, k = 0;
+	temp = malloc(sizeof(int) * size);
+	if (temp == NULL)
+		return;
 
-	while (i < size / 2 && j < size - (size / 2))
-	{
-		if (array[i] < array[j + (size / 2)])
-		{
-			temp[k] = array[i];
-			i++;
-		}
-		else
-		{
-			temp[k] = array[j + (size / 2)];
-			j++;
-		}
-		k++;
-	}
-	while (i < size / 2)
-	{
-		temp[k] = array[i];
-		k++;
-		i++;
-	}
-	while (j < size - (size / 2))
-	{
-		temp[k] = array[j + (size / 2)];
-		k++;
-		j++;
-	}
 	for (i = 0; i < size; i++)
+		temp[i] = array[i];
+
+	for (i = 0, j = size / 2, k = 0; k < size; k++)
 	{
-		array[i] = temp[i];
+		if (j >= size)
+			array[k] = temp[i++];
+		else if (i >= size / 2)
+			array[k] = temp[j++];
+		else if (temp[i] < temp[j])
+			array[k] = temp[i++];
+		else
+			array[k] = temp[j++];
 	}
 	free(temp);
 }
--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -9,6 +9,6 @@ void merge_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 
 
-void merge(int *array, size_t size)
+void merge(int *array, size_t size);
 
 #endif
