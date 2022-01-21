71c643f6850d56fb23d88c854d3490456cd37b43
valerienierenberg <valerie.nierenberg@gmail.com>
2022-01-20 18:57:05 -0600

    Add

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -1,72 +1,36 @@
 #include "sort.h"
 
 /**
-* merge - Merges two sorted arrays
-* @array: The array to be sorted
-* @temp: Temporary array to hold sorted values
-* @start: The starting index of the array
-* @mid: The middle index of the array
-* @end: The ending index of the array
-* Return: Nothing
-*/
-void merge(int *array, int *temp, size_t start, size_t mid, size_t end)
-{
-	size_t i, j, k;
-
-	i = start;
-	j = mid + 1;
-	k = 0;
-	while (i <= mid && j <= end)
-	{
-		if (array[i] < array[j])
-			temp[k++] = array[i++];
-		else
-			temp[k++] = array[j++];
-	}
-	while (i <= mid)
-		temp[k++] = array[i++];
-	while (j <= end)
-		temp[k++] = array[j++];
-	for (i = 0; i < k; i++)
-        array[start + i] = temp[i];
-}
-
-/**
-* merge_sort_rec - Helper function for merge sort
-* @array: The array to be sorted
-* @start: The starting index of the array
-* @end: The ending index of the array
-* Return: Nothing
-*/
-void merge_sort_rec(int *array, size_t start, size_t end)
-{
-	int *temp;
-	size_t mid;
-
-	if (start >= end)
-		return;
-
-	mid = (start + end) / 2;
-	merge_sort_rec(array, start, mid);
-	merge_sort_rec(array, mid + 1, end);
-	temp = malloc(sizeof(int) * (end - start + 1));
-	if (temp == NULL)
-		return;
-	merge(array, temp, start, mid, end);
-	free(temp);
-}
-
-/**
-* merge_sort - Sorts an array of integers in ascending order using the
-* Merge sort algorithm
-* @array: The array to be sorted
-* @size: Number of elements in @array
-* Return: Nothing
+* sorts an array of ints using top-down merge sort algorithm
+* @array: array to be sorted
+* @size: size of array
 */
 void merge_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
 
-	merge_sort_rec(array, 0, size - 1);
+	merge_sort(array, size / 2);
+	merge_sort(array + size / 2, size - size / 2);
+	merge(array, size);
+}
+
+/**
+* merges two sorted arrays into one
+* @array: array to be merged
+* @size: size of array
+*/
+void merge(int *array, size_t size)
+{
+	int *temp = malloc(sizeof(int) * size);
+	int i = 0, j = size / 2, k = 0;
+
+	if (!temp)
+		return;
+
+	while (i < size / 2 && j < size)
+		temp[k++] = array[i] < array[j] ? array[i++] : array[j++];
+
+	while (i < size / 2)
+		temp[k++] = array[i++];
 }
--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -8,4 +8,7 @@
 void merge_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 
+
+void merge(int *array, size_t size)
+
 #endif
