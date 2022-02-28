4d43776826d83d413305ebcec6e0fb7de4f07b37
valerienierenberg <valerie.nierenberg@gmail.com>
2022-02-28 08:57:47 -0600

    Add task

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,27 +1,35 @@
 #include "holberton.h"
 
 /**
- * wildcmp - compares two strings. S2 can contain wildcards.
- * @s1: string 1
- * @s2: string 2
- *
- * Return: 1 if strings match, 0 if not
- */
+* wildcmp - compares two strings and returns 1 if the strings can be considered identical, 0 otherwise
+* @s1: string 1
+* @s2: string 2
+* Return: 1 if the strings can be considered identical, 0 otherwise
+*/
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	else if (*s1 == '\0' && *s2 != '\0')
-		return (0);
-	else if (*s1 != '\0' && *s2 == '\0')
-		return (0);
-	else if (*s1 == '*' && *s2 == '*')
-		return (wildcmp(s1 + 1, s2 + 1));
-	else if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	else if (*s1 == '*' && *s2 != '\0')
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	else
-		return (0);
+    int i = 0, j = 0;
+
+    while (s1[i] != '\0')
+    {
+        if (s2[j] == '*')
+        {
+            while (s2[j] == '*')
+                j++;
+            while (s1[i] != s2[j] && s1[i] != '\0')
+                i++;
+            if (s1[i] == '\0')
+                return (0);
+            j++;
+            i++;
+        }
+        else if (s1[i] != s2[j])
+            return (0);
+        else
+            i++, j++;
+    }
+    if (s2[j] != '\0')
+        return (0);
+    return (1);
 }
