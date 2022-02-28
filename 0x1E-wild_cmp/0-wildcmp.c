51de21473a1f376c38b5399612ca7c7525a5581d
valerienierenberg <valerie.nierenberg@gmail.com>
2022-02-28 09:01:00 -0600

    Add task

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 
 /**
-* wildcmp - compares two strings. s2 can contain the special character '*' which can replace any string (including empty string)
+* wildcmp - compares two strings. s2 can contain the special character '*'
+* which can replace any string (including empty string)
 * @s1: string 1
 * @s2: string 2
 * Return: 1 if the strings can be considered identical, 0 otherwise
