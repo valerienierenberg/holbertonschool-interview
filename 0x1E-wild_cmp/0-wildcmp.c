0bc00906d3801f470a3d66bbc0f863a8607f0946
valerienierenberg <valerie.nierenberg@gmail.com>
2022-02-28 09:00:04 -0600

    Add task

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-* wildcmp - compares two strings and returns 1 if the strings can be considered identical, 0 otherwise
+* wildcmp - compares two strings. s2 can contain the special character '*' which can replace any string (including empty string)
 * @s1: string 1
 * @s2: string 2
 * Return: 1 if the strings can be considered identical, 0 otherwise
@@ -9,27 +9,29 @@
 
 int wildcmp(char *s1, char *s2)
 {
-    int i = 0, j = 0;
-
-    while (s1[i] != '\0')
-    {
-        if (s2[j] == '*')
-        {
-            while (s2[j] == '*')
-                j++;
-            while (s1[i] != s2[j] && s1[i] != '\0')
-                i++;
-            if (s1[i] == '\0')
-                return (0);
-            j++;
-            i++;
-        }
-        else if (s1[i] != s2[j])
-            return (0);
-        else
-            i++, j++;
-    }
-    if (s2[j] != '\0')
-        return (0);
-    return (1);
+	if (*s2 == '\0')
+	{
+		if (*s1 == '\0')
+			return (1);
+		else
+			return (0);
+	}
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+			return (1);
+		else
+		{
+			while (*s1 != '\0')
+			{
+				if (wildcmp(s1, s2 + 1))
+					return (1);
+				s1++;
+			}
+			return (0);
+		}
+	}
+	if (*s1 != *s2 && *s2 != '?')
+		return (0);
+	return (wildcmp(s1 + 1, s2 + 1));
 }
