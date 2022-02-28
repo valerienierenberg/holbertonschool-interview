14453c1d204f219b878b6d60edf92fd34cea165e
valerienierenberg <valerie.nierenberg@gmail.com>
2022-02-28 08:56:09 -0600

    Add task

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -16,16 +16,12 @@ int wildcmp(char *s1, char *s2)
 		return (0);
 	else if (*s1 != '\0' && *s2 == '\0')
 		return (0);
-	else if (*s1 != '\0' && *s2 != '\0')
-	{
-		if (*s2 == '*')
-			return (wildcmp(s1, s2 + 1));
-		else if (*s1 == *s2)
-			return (wildcmp(s1 + 1, s2 + 1));
-		else if (*s2 == '?')
-			return (wildcmp(s1 + 1, s2 + 1));
-		else
-			return (0);
-	}
-	return (0);
+	else if (*s1 == '*' && *s2 == '*')
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s1 == '*' && *s2 != '\0')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	else
+		return (0);
 }
