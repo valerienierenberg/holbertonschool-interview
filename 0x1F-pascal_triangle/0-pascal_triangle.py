8b05616e06f56eb5209c3ddf43217534e3a4532c
valerienierenberg <valerie.nierenberg@gmail.com>
2022-04-11 22:41:28 +0000

    pep8

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,7 @@
 """ function def pascal_triangle(n): that returns a list of lists of integers
 representing the Pascal’s triangle of n """
 
+
 def pascal_triangle(n):
     """ function def pascal_triangle(n): that returns a list of lists of integers
     representing the Pascal’s triangle of n """
